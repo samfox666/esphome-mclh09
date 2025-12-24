110e849a64121c8434d2dfecc5c45738e8e17c53
samfox666 <56519026+samfox666@users.noreply.github.com>
2025-12-25 00:39:38 +0300

    Update __init__.py

--- components/mclh_09/__init__.py
+++ components/mclh_09/__init__.py
@@ -12,10 +12,6 @@ CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(MCLH09),
         cv.Required("ble_client_id"): cv.use_id(ble_client.BLEClient),  # <-- строка
-        cv.Optional("temperature"): cv.Schema({}),  # <-- добавим пустую схему
-        cv.Optional("humidity"): cv.Schema({}),     # <-- добавим пустую схему
-        cv.Optional("illuminance"): cv.Schema({}),  # <-- добавим пустую схему
-        cv.Optional("battery_level"): cv.Schema({}) # <-- добавим пустую схему
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
@@ -25,4 +21,3 @@ async def to_code(config):
 
     paren = await cg.get_variable(config["ble_client_id"])
     cg.add(var.set_ble_client_parent(paren))
-    
