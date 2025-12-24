432b7e51655b8b54a1e99f922878444193fb8056
samfox666 <56519026+samfox666@users.noreply.github.com>
2025-12-24 23:07:15 +0300

    Update __init__.py

--- components/mclh_09/__init__.py
+++ components/mclh_09/__init__.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import CONF_ID, CONF_BLE_CLIENT_ID
+from esphome.const import CONF_ID
 
 # Namespace
 mclh_ns = cg.esphome_ns.namespace("mclh_09")
@@ -10,7 +10,7 @@ MCLH09 = mclh_ns.class_("MCLH09", cg.Component)
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(MCLH09),
-        cv.Required(CONF_BLE_CLIENT_ID): cv.use_id("ble_client.BLEClient"),  # <-- строка
+        cv.Required("ble_client_id"): cv.use_id("ble_client.BLEClient"),  # <-- строка
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
@@ -20,7 +20,7 @@ async def to_code(config):
     await cg.register_component(var, config)
 
     # Получаем BLE-клиента
-    paren = await cg.get_variable(config[CONF_BLE_CLIENT_ID])
+    paren = await cg.get_variable(config["ble_client_id"])
     cg.add(var.set_ble_client(paren))
 
 # Экспортируем MCLH09 и mclh_ns для sensor.py
