3f36cb5d25582a4898d4946acb72dd65d5d1efd2
samfox666 <56519026+samfox666@users.noreply.github.com>
2025-12-24 18:52:51 +0300

    Update mclh_09.h

--- components/mclh_09/mclh_09.h
+++ components/mclh_09/mclh_09.h
@@ -13,10 +13,6 @@ class MCLH09 : public Component, public esp32_ble_tracker::ESPBTDeviceListener {
  public:
   void set_address(uint64_t address) { address_ = address; }
   void set_temperature_sensor(sensor::Sensor* temperature) { temperature_sensor_ = temperature; }
-  void set_humidity_sensor(sensor::Sensor* humidity) { humidity_sensor_ = humidity; }
-  void set_illuminance_sensor(sensor::Sensor* illuminance) { illuminance_sensor_ = illuminance; }
-  void set_soil_moisture_sensor(sensor::Sensor* soil_moisture) { soil_moisture_sensor_ = soil_moisture; }
-  void set_battery_level_sensor(sensor::Sensor* battery_level) { battery_level_sensor_ = battery_level; }
 
   void dump_config() override;
   float get_setup_priority() const override { return setup_priority::DATA; }
@@ -27,10 +23,6 @@ class MCLH09 : public Component, public esp32_ble_tracker::ESPBTDeviceListener {
  protected:
   uint64_t address_{0};
   sensor::Sensor* temperature_sensor_{nullptr};
-  sensor::Sensor* humidity_sensor_{nullptr};
-  sensor::Sensor* illuminance_sensor_{nullptr};
-  sensor::Sensor* soil_moisture_sensor_{nullptr};
-  sensor::Sensor* battery_level_sensor_{nullptr};
 
   bool parse_device_(const esp32_ble_tracker::ESPBTDevice &device);
 };
