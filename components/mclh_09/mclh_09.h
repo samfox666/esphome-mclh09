cbf6f00d2dd69dce3aefd1e2f002b03c6fc75b87
samfox666 <56519026+samfox666@users.noreply.github.com>
2025-12-24 19:33:37 +0300

    Update mclh_09.h

--- components/mclh_09/mclh_09.h
+++ components/mclh_09/mclh_09.h
@@ -2,14 +2,14 @@
 
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
-#include "esphome/components/ble_client/ble_client.h"
+#include "esphome/components/esp32_ble_tracker/esp32_ble_tracker.h"
 
 #ifdef USE_ESP32
 
 namespace esphome {
 namespace mclh_09 {
 
-class MCLH09 : public ble_client::BLEClientBase {
+class MCLH09 : public esp32_ble_tracker::ESPBTDeviceListener, public Component {
  public:
   void set_address(uint64_t address) { address_ = address; }
   void set_temperature_sensor(sensor::Sensor* temperature) { temperature_sensor_ = temperature; }
@@ -17,14 +17,12 @@ class MCLH09 : public ble_client::BLEClientBase {
   void dump_config() override;
   float get_setup_priority() const override { return setup_priority::DATA; }
 
-  // Новый метод — вызывается при BLE-обнаружении
-  void on_ble_advertise(esp32_ble_tracker::ESPBTDevice &device) override;
+  // Старый метод — вызывается при BLE-обнаружении
+  bool parse_device(const esp32_ble_tracker::ESPBTDevice &device) override;
 
  protected:
   uint64_t address_{0};
   sensor::Sensor* temperature_sensor_{nullptr};
-
-  bool parse_device_(const esp32_ble_tracker::ESPBTDevice &device);
 };
 
 }  // namespace mclh_09
