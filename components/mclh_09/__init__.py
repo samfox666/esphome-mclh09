574e365ecb1f4e5229bb71e9afc88f8f5fdab3c6
samfox666 <56519026+samfox666@users.noreply.github.com>
2025-12-25 00:18:07 +0300

    Update __init__.py

--- components/mclh_09/__init__.py
+++ components/mclh_09/__init__.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import CONF_ID
+from esphome.const import CONF_ID, CONF_MAC_ADDRESS
 
 # Namespace
 mclh_ns = cg.esphome_ns.namespace("mclh_09")
@@ -10,7 +10,7 @@ MCLH09 = mclh_ns.class_("MCLH09", cg.Component)
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(MCLH09),
-        cv.Required("ble_client_id"): cv.use_id("ble_client.BLEClient"),  # <-- строка
+        cv.Required(CONF_MAC_ADDRESS): cv.mac_address,
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
@@ -19,9 +19,8 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-    # Получаем BLE-клиента
-    paren = await cg.get_variable(config["ble_client_id"])
-    cg.add(var.set_ble_client(paren))
+    # Устанавливаем MAC-адрес
+    cg.add(var.set_address(config[CONF_MAC_ADDRESS].as_hex))
 
 # Экспортируем MCLH09 и mclh_ns для sensor.py
 mclh_09_ns = mclh_ns
