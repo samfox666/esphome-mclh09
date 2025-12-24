0264c90e4e13e1feca7591198baf865dbd267cb3
samfox666 <56519026+samfox666@users.noreply.github.com>
2025-12-25 00:31:19 +0300

    Update __init__.py

--- components/mclh_09/__init__.py
+++ components/mclh_09/__init__.py
@@ -1,26 +1,24 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import CONF_ID, CONF_MAC_ADDRESS
+from esphome.components import ble_client
+from esphome.const import CONF_ID, CONF_BLE_CLIENT_ID
 
 # Namespace
 mclh_ns = cg.esphome_ns.namespace("mclh_09")
 MCLH09 = mclh_ns.class_("MCLH09", cg.Component)
 
-# Определяем схему конфигурации
+# Схема
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(MCLH09),
-        cv.Required(CONF_MAC_ADDRESS): cv.mac_address,
+        cv.Required(CONF_BLE_CLIENT_ID): cv.use_id(ble_client.BLEClient),
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
-# Основная функция для генерации кода
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-    # Устанавливаем MAC-адрес
-    cg.add(var.set_address(config[CONF_MAC_ADDRESS].as_hex))
-
-# Экспортируем MCLH09 и mclh_ns для sensor.py
-mclh_09_ns = mclh_ns
+    paren = await cg.get_variable(config[CONF_BLE_CLIENT_ID])
+    cg.add(var.set_ble_client_parent(paren))
+    
