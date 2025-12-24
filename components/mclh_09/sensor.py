4866a3519f3a1d1ab4506e51aa58754d8d2f65a3
samfox666 <56519026+samfox666@users.noreply.github.com>
2025-12-24 07:08:48 +0300

    Update sensor.py

--- components/mclh_09/sensor.py
+++ components/mclh_09/sensor.py
@@ -20,57 +20,24 @@ from . import MCLH09, mclh_09_ns
 
 DEPENDENCIES = ["mclh_09"]
 
-CONF_UPDATE_INTERVAL = "update_interval"
+CONF_MCLH_09_ID = "mclh_09_id"
 CONF_SOIL_MOISTURE = "soil_moisture"
+CONF_UPDATE_INTERVAL = "update_interval"
 
-SENSOR_TYPES = {
-    CONF_TEMPERATURE: sensor.sensor_schema(
-        unit_of_measurement=UNIT_CELSIUS,
-        accuracy_decimals=1,
-        device_class=DEVICE_CLASS_TEMPERATURE,
-        state_class=STATE_CLASS_MEASUREMENT,
-    ),
-    CONF_HUMIDITY: sensor.sensor_schema(
-        unit_of_measurement=UNIT_PERCENT,
-        accuracy_decimals=0,
-        device_class=DEVICE_CLASS_HUMIDITY,
-        state_class=STATE_CLASS_MEASUREMENT,
-    ),
-    CONF_BATTERY_LEVEL: sensor.sensor_schema(
-        unit_of_measurement=UNIT_PERCENT,
-        accuracy_decimals=0,
-        device_class=DEVICE_CLASS_BATTERY,
-        state_class=STATE_CLASS_MEASUREMENT,
-    ),
-    CONF_SOIL_MOISTURE: sensor.sensor_schema(
-        unit_of_measurement=UNIT_PERCENT,
-        accuracy_decimals=0,
-        state_class=STATE_CLASS_MEASUREMENT,
-    ),
-    CONF_ILLUMINANCE: sensor.sensor_schema(
-        unit_of_measurement=UNIT_LUX,
-        accuracy_decimals=0,
-        device_class=DEVICE_CLASS_ILLUMINANCE,
-        state_class=STATE_CLASS_MEASUREMENT,
-    ),
-}
-
-CONFIG_SCHEMA = cv.All(
-    cv.Schema(
-        {
-            cv.GenerateID(): cv.use_id(MCLH09),
-            cv.Optional(CONF_TEMPERATURE): sensor.sensor_schema(),
-            cv.Optional(CONF_HUMIDITY): sensor.sensor_schema(),
-            cv.Optional(CONF_BATTERY_LEVEL): sensor.sensor_schema(),
-            cv.Optional(CONF_SOIL_MOISTURE): sensor.sensor_schema(),
-            cv.Optional(CONF_ILLUMINANCE): sensor.sensor_schema(),
-            cv.Optional(CONF_UPDATE_INTERVAL, default="15min"): cv.update_interval,
-        }
-    ).extend(cv.COMPONENT_SCHEMA)
-)
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(CONF_MCLH_09_ID): cv.use_id(MCLH09),
+        cv.Optional(CONF_TEMPERATURE): sensor.sensor_schema(),
+        cv.Optional(CONF_HUMIDITY): sensor.sensor_schema(),
+        cv.Optional(CONF_BATTERY_LEVEL): sensor.sensor_schema(),
+        cv.Optional(CONF_SOIL_MOISTURE): sensor.sensor_schema(),
+        cv.Optional(CONF_ILLUMINANCE): sensor.sensor_schema(),
+        cv.Optional(CONF_UPDATE_INTERVAL, default="15min"): cv.update_interval,
+    }
+).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
-    parent = await cg.get_variable(config[CONF_ID])
+    parent = await cg.get_variable(config[CONF_MCLH_09_ID])
 
     if CONF_TEMPERATURE in config:
         sens = await sensor.new_sensor(config[CONF_TEMPERATURE])
