c993e86871340a43b3ca9a10743b43ee04f1af71
samfox666 <56519026+samfox666@users.noreply.github.com>
2025-12-24 19:58:49 +0300

    Update mclh_09.h

--- components/mclh_09/mclh_09.h
+++ components/mclh_09/mclh_09.h
@@ -9,20 +9,28 @@
 namespace esphome {
 namespace mclh_09 {
 
-class MCLH09 : public esp32_ble_tracker::ESPBTDeviceListener, public Component {
+class MCLH09 : public Component, public esp32_ble_tracker::ESPBTDeviceListener {
  public:
   void set_address(uint64_t address) { address_ = address; }
   void set_temperature_sensor(sensor::Sensor* temperature) { temperature_sensor_ = temperature; }
+  void set_humidity_sensor(sensor::Sensor* humidity) { humidity_sensor_ = humidity; }
+  void set_illuminance_sensor(sensor::Sensor* illuminance) { illuminance_sensor_ = illuminance; }
+  void set_soil_moisture_sensor(sensor::Sensor* soil_moisture) { soil_moisture_sensor_ = soil_moisture; }
+  void set_battery_level_sensor(sensor::Sensor* battery_level) { battery_level_sensor_ = battery_level; }
 
   void dump_config() override;
   float get_setup_priority() const override { return setup_priority::DATA; }
 
-  // Старый метод — вызывается при BLE-обнаружении
+  // Новый метод — вызывается при BLE-обнаружении
   bool parse_device(const esp32_ble_tracker::ESPBTDevice &device) override;
 
  protected:
   uint64_t address_{0};
   sensor::Sensor* temperature_sensor_{nullptr};
+  sensor::Sensor* humidity_sensor_{nullptr};
+  sensor::Sensor* illuminance_sensor_{nullptr};
+  sensor::Sensor* soil_moisture_sensor_{nullptr};
+  sensor::Sensor* battery_level_sensor_{nullptr};
 };
 
 }  // namespace mclh_09
