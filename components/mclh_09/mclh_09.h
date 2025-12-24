bbf62b8b4b6c6893d4e70bd0a4d51fafb2eb9a25
samfox666 <56519026+samfox666@users.noreply.github.com>
2025-12-25 02:59:35 +0300

    Update mclh_09.h

--- components/mclh_09/mclh_09.h
+++ components/mclh_09/mclh_09.h
@@ -20,6 +20,7 @@ class MCLH09 : public Component, public ble_client::BLEClientNode {
   void dump_config() override;
   float get_setup_priority() const override { return setup_priority::DATA; }
 
+  // Новый метод — вызывается при BLE-событиях
   void gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if,
                            esp_ble_gattc_cb_param_t *param) override;
 
