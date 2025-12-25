17c7e55b58cd0ff92e11dc689f8b7d105b55f9d4
samfox666 <56519026+samfox666@users.noreply.github.com>
2025-12-25 05:06:27 +0300

    Update mclh_09.h

--- components/mclh_09/mclh_09.h
+++ components/mclh_09/mclh_09.h
@@ -2,34 +2,37 @@
 
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
-#include "esphome/components/ble_client/ble_client.h"
+#include "esphome/components/esp32_ble_tracker/esp32_ble_tracker.h"
 
 #ifdef USE_ESP32
 
 namespace esphome {
 namespace mclh_09 {
 
-class MCLH09 : public Component, public ble_client::BLEClientNode {
+class MCLH09 : public Component, public esp32_ble_tracker::ESPBTDeviceListener {
  public:
+  void set_address(uint64_t address) { address_ = address; }
   void set_temperature_sensor(sensor::Sensor* temperature) { temperature_sensor_ = temperature; }
   void set_humidity_sensor(sensor::Sensor* humidity) { humidity_sensor_ = humidity; }
   void set_illuminance_sensor(sensor::Sensor* illuminance) { illuminance_sensor_ = illuminance; }
   void set_soil_moisture_sensor(sensor::Sensor* soil_moisture) { soil_moisture_sensor_ = soil_moisture; }
   void set_battery_level_sensor(sensor::Sensor* battery_level) { battery_level_sensor_ = battery_level; }
+  void set_rssi_sensor(sensor::Sensor* rssi) { rssi_sensor_ = rssi; }
 
   void dump_config() override;
   float get_setup_priority() const override { return setup_priority::DATA; }
 
-  // Новый метод — вызывается при BLE-событиях
-  void gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if,
-                           esp_ble_gattc_cb_param_t *param) override;
+  // Новый метод — вызывается при BLE-обнаружении
+  void on_ble_advertise(esp32_ble_tracker::ESPBTDevice &device) override;
 
  protected:
+  uint64_t address_{0};
   sensor::Sensor* temperature_sensor_{nullptr};
   sensor::Sensor* humidity_sensor_{nullptr};
   sensor::Sensor* illuminance_sensor_{nullptr};
   sensor::Sensor* soil_moisture_sensor_{nullptr};
   sensor::Sensor* battery_level_sensor_{nullptr};
+  sensor::Sensor* rssi_sensor_{nullptr};
 };
 
 }  // namespace mclh_09
