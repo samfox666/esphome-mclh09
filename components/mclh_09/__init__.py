03e2d0a35767d87ab5f608c8652a36d49d685fd7
samfox666 <56519026+samfox666@users.noreply.github.com>
2025-12-25 10:52:29 +0300

    Update __init__.py

--- components/mclh_09/__init__.py
+++ components/mclh_09/__init__.py
@@ -1,8 +1,12 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import esp32_ble_tracker
+from esphome.components import esp32_ble_tracker, myhomeiot_ble_host
 from esphome.const import (
     CONF_ID,
+    CONF_MAC_ADDRESS,
+    CONF_ERROR_COUNTING,
+    CONF_RAW_SOIL,
+    CONF_BLE_HOST,
 )
 
 # Namespace
@@ -13,15 +17,21 @@ MCLH09 = mclh_ns.class_("MCLH09", cg.Component)
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(MCLH09),
-        cv.Required("mac_addresses"): cv.ensure_list(cv.mac_address),  # <-- строка
+        cv.Required(CONF_MAC_ADDRESS): cv.ensure_list(cv.mac_address),
+        cv.Optional(CONF_ERROR_COUNTING, default=False): cv.boolean,
+        cv.Optional(CONF_RAW_SOIL, default=False): cv.boolean,
+        cv.Required(CONF_BLE_HOST): cv.use_id(myhomeiot_ble_host.MyHomeIOT_BLEHost),
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
 # Основная функция для генерации кода
 async def to_code(config):
-    mac_addrs = [cg.RawExpression(f"0x{addr.replace(':', '')}") for addr in config["mac_addresses"]]  # <-- строка
-    
-    var = cg.new_Pvariable(config[CONF_ID], mac_addrs)
+    addr_list = []
+    for it in config[CONF_MAC_ADDRESS]:
+      addr_list.append(it.as_hex)
+    var = cg.new_Pvariable(config[CONF_ID], addr_list, config[CONF_ERROR_COUNTING], config[CONF_RAW_SOIL])
+    ble_host = await cg.get_variable(config[CONF_BLE_HOST])
+    cg.add(var.set_ble_host(ble_host))
     await cg.register_component(var, config)
 
     # Регистрируем BLE-компонент
