f49e539e89de39b1c8f805a082822085219b155d
samfox666 <56519026+samfox666@users.noreply.github.com>
2025-12-25 10:45:36 +0300

    Update __init__.py

--- components/mclh_09/__init__.py
+++ components/mclh_09/__init__.py
@@ -3,30 +3,23 @@ import esphome.config_validation as cv
 from esphome.components import esp32_ble_tracker
 from esphome.const import (
     CONF_ID,
-    CONF_MAC_ADDRESSES,
 )
 
 # Namespace
-# mclh_ns = cg.esphome_ns.namespace("mclh_09")
-# MCLH09 = mclh_ns.class_("MCLH09", cg.Component)
-
-mclh_09_ns = cg.esphome_ns.namespace("mclh_09")
-Mclh09Array = mclh_09_ns.class_(
-    "Mclh09Array", cg.Component
-)
-Mclh09ArrayForceUpdateAction = mclh_09_ns.class_("Mclh09ArrayForceUpdateAction", automation.Action)
+mclh_ns = cg.esphome_ns.namespace("mclh_09")
+MCLH09 = mclh_ns.class_("MCLH09", cg.Component)
 
 # Определяем схему конфигурации
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(MCLH09),
-        cv.Required(CONF_MAC_ADDRESSES): cv.ensure_list(cv.mac_address),
+        cv.Required("mac_addresses"): cv.ensure_list(cv.mac_address),  # <-- строка
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
 # Основная функция для генерации кода
 async def to_code(config):
-    mac_addrs = [cg.RawExpression(f"0x{addr.replace(':', '')}") for addr in config[CONF_MAC_ADDRESSES]]
+    mac_addrs = [cg.RawExpression(f"0x{addr.replace(':', '')}") for addr in config["mac_addresses"]]  # <-- строка
     
     var = cg.new_Pvariable(config[CONF_ID], mac_addrs)
     await cg.register_component(var, config)
